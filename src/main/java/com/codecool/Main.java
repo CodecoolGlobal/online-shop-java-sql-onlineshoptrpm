e3a979ee29772311357f3b6e28195b91d0d23d09
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-05-05 15:37:13 +0200

    switch between display admin and customer add

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -1,14 +1,34 @@
 package com.codecool;
 
+import com.codecool.dao.UserDao;
+import com.codecool.models.User;
+import java.util.Scanner;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test");
-
         UI ui = new UI();
+        UserDao userDao = new UserDao();
+        //List<User> users = userDao.getUsers();
 
-        ui.printMenu();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Login: ");
+        String login = scanner.nextLine();
+        System.out.println("Enter Password: ");
+        String password = scanner.nextLine();
 
+        User user = userDao.getUser(login, password);
+        switch (user.getRole()) {
+            case 1:
+                ui.displayAdminMenu();
+                break;
+            case 2:
+                ui.displayCustomerMenu();
+                break;
+        }
+
+        System.out.println(user.getName() + " " + user.getSurname());
 
     }
 }
