a96d0290058622a39e9531b88980c58ad21eab56
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-05-05 19:32:13 +0200

    productDao refactor

--- src/main/java/com/codecool/dao/ProductDao.java
+++ src/main/java/com/codecool/dao/ProductDao.java
@@ -11,19 +11,10 @@ public class ProductDao extends Dao {
     public List<Product> getProducts() {
         List<Product> products = new ArrayList<>();
         connect();
-
         try {
             ResultSet results = statement.executeQuery("SELECT * FROM Products;");
             while (results.next()) {
-                int id = results.getInt("id");
-                String name = results.getString("name");
-                float price = results.getFloat("price");
-                int amount = results.getInt("amount");
-                boolean is_available = results.getInt("is_available") == 1;
-                int category_id = results.getInt("category_id");
-
-                Product product = new Product(id, name, price, amount, is_available, category_id);
-                products.add(product);
+                products.add(createProduct(results));
             }
             results.close();
             statement.close();
@@ -33,4 +24,17 @@ public class ProductDao extends Dao {
         }
         return products;
     }
+
+    private Product createProduct(ResultSet results) throws SQLException {
+        int id = results.getInt("id");
+        String name = results.getString("name");
+        float price = results.getFloat("price");
+        int amount = results.getInt("amount");
+        boolean is_available = results.getInt("is_available") == 1;
+        int category_id = results.getInt("category_id");
+        return new Product(id, name, price, amount, is_available, category_id);
+    }
+
+
+
 }
