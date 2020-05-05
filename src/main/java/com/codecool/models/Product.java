2201994c9116853a2b2201b11a94f909f482c46c
Tomek Krogulec <tomek.krogulec@o2.pl>
2020-05-05 10:38:27 +0200

    added basic classes

--- src/main/java/com/codecool/models/Product.java
+++ src/main/java/com/codecool/models/Product.java
@@ -1,4 +1,65 @@
 package com.codecool.models;
 
 public class Product {
+    private int id;
+    private String name;
+    private float price;
+    private int amount;
+    private boolean isAvailable;
+    private Category category;
+
+    public Product(String name, float price, int amount, Category category) {
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
