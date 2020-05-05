9b68b36cb2b97495ecf25059579bba0e91f24222
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-05-05 23:35:24 +0200

    main function deep refactor

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -1,32 +1,11 @@
 package com.codecool;
 
-import com.codecool.dao.UserDao;
-import com.codecool.models.User;
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
-        UI ui = new UI();
-        IO io = new IO();
-        UserDao userDao = new UserDao();
-        //List<User> users = userDao.getUsers();
-
-        String email = io.gatherInput("Enter Email: ");
-        String password = io.gatherInput("Enter Password: ");
-
-
-        User user = userDao.getUser(email, password);
-        switch (user.getRole()) {
-            case 1:
-                ui.displayAdminMenu();
-                break;
-            case 2:
-                ui.displayCustomerMenu();
-                break;
+        MenuHandler menuHandler = new MenuHandler();
+        while (menuHandler.isRunning) {
+            menuHandler.mainMenu();
         }
-
-        System.out.println(user.getName() + " " + user.getEmail() + " " + user.getPhoneNumber());
-
     }
 }
