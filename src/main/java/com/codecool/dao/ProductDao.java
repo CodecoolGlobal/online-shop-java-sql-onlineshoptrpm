cd43796fdfb9a63493d3d75e3edb0fe6de2f4baa
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-05-05 12:04:30 +0200

    getProducts method refactor

--- src/main/java/com/codecool/dao/ProductDao.java
+++ src/main/java/com/codecool/dao/ProductDao.java
@@ -1,5 +1,6 @@
 package com.codecool.dao;
 
+import com.codecool.models.Category;
 import com.codecool.models.Product;
 import java.sql.*;
 import java.util.ArrayList;
@@ -16,12 +17,13 @@ public class ProductDao extends Dao {
             while (results.next()) {
                 int id = results.getInt("id");
                 String name = results.getString("name");
-                String price = results.getString("price");
+                float price = results.getFloat("price");
                 int amount = results.getInt("amount");
-                int is_available = results.getInt("is_available");
+                boolean is_available = results.getInt("is_available") == 1;
                 int category_id = results.getInt("category_id");
+                Category category = new Category("Name?");//todo function creating Category object based on category_id?
 
-                Product product = new Product(id, name, price, amount, is_available, category_id);
+                Product product = new Product(id, name, price, amount, is_available, category);
                 products.add(product);
             }
             results.close();
@@ -31,9 +33,5 @@ public class ProductDao extends Dao {
             e.printStackTrace();
         }
         return products;
-    }
-
-    private boolean is_availableToBool(int i){
-        return i == 1;
     }
 }
