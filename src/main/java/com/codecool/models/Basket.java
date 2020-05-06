71bdf810c0829488237f2f1025f138d68529da16
Maciek <mcyran1996@gmail.com>
2020-05-06 15:19:58 +0200

    basket constructor added

--- src/main/java/com/codecool/models/Basket.java
+++ src/main/java/com/codecool/models/Basket.java
@@ -5,13 +5,21 @@ import java.util.ArrayList;
 public class Basket {
     private int id;
     private ArrayList<Product> products;
+    private int productId;
+    private int orderId;
+    private int quantity;
 
-    public Basket(int id, ArrayList<Product> products) {
-        this.id = id;
-        this.products = products;
-    }
+//    public Basket(int id, ArrayList<Product> products) {
+//        this.id = id;
+//        this.products = products;
+//    }
 
     public Basket(int id, int productId, int orderId, int quantity) {
-
+        this.id = id;
+        this.productId = productId;
+        this.orderId = orderId;
+        this.quantity = quantity;
     }
+
+
 }
