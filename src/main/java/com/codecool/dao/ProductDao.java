ac4fab2665c3b48aab760964a1cd8659cbba2b70
Maciek <mcyran1996@gmail.com>
2020-05-06 17:00:24 +0200

    adding product by admin

--- src/main/java/com/codecool/dao/ProductDao.java
+++ src/main/java/com/codecool/dao/ProductDao.java
@@ -1,5 +1,6 @@
 package com.codecool.dao;
 
+import com.codecool.IO;
 import com.codecool.models.Category;
 import com.codecool.models.Product;
 import java.sql.*;
@@ -36,8 +37,33 @@ public class ProductDao extends Dao {
     }
 
     public void addNewProduct(){
-        //temp method
-        System.out.println("here will be a method adding new product to database");
+        IO io = new IO();
+        System.out.println("You're adding new product to data base");
+        String newName = io.gatherInput("Enter name of new product: ");
+        int newPrice = io.gatherIntInput("Enter new price of the product: ", 99999);
+        int newAmount = io.gatherIntInput("Enter new amount of the product: ", 99999);
+        int isNewAvailable = io.gatherIntInput("Is new product available? ", 1);
+        int newCategory = io.gatherIntInput("What is category of new product? ", 7);
+//        int newRating = 0;
+//        int newNoRates = 0;
+        connect();
+        PreparedStatement addNewProduct;
+        String sql = "INSERT INTO Product (id, name, price, amount, is_available, category_id, rating, no_rates) VALUES (?, ?, ?, ?, ?, 0, 0)";
+        try {
+            addNewProduct = connection.prepareStatement(sql);
+            addNewProduct.setString(1,newName);
+            addNewProduct.setInt(2,newPrice);
+            addNewProduct.setInt(3,newAmount);
+            addNewProduct.setInt(4,isNewAvailable);
+            addNewProduct.setInt(5,newCategory);
+            addNewProduct.setInt(6,0);
+            addNewProduct.setInt(7,0);
+            addNewProduct.executeUpdate();
+            addNewProduct.close();
+            connection.close();
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 }
