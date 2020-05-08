2f048f2fecdf8259bd2aa3978430457192a65450
Tomek Krogulec <tomek.krogulec@o2.pl>
2020-05-08 09:34:30 +0200

    refactor

--- src/main/java/com/codecool/dao/BasketDao.java
+++ src/main/java/com/codecool/dao/BasketDao.java
@@ -13,60 +13,6 @@ import java.util.List;
 
 public class BasketDao extends Dao {
 
-    private final ProductDao productDao;
-
-    public BasketDao(){
-        this.productDao = new ProductDao();
-    }
-
-    public List<Basket> getBaskets() {
-        List<Basket> baskets = new ArrayList<>();
-        connect();
-        try {
-            ResultSet results = statement.executeQuery("SELECT * FROM Baskets;");
-            while (results.next()) {
-                baskets.add(createBasket(results));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return baskets;
-    }
-
-    private Basket createBasket(ResultSet results) throws SQLException {
-        int id = results.getInt("id");
-        int productId = results.getInt("product_is");
-        int orderId = results.getInt("order_id");
-        int quantity = results.getInt("quantity");
-        return new Basket(id, productId, orderId, quantity);
-    }
-
-    public Basket getBasket(int order_id) throws SQLException{
-        List<Product> productsInBasket = new ArrayList<>();
-        connect();
-        try {
-            PreparedStatement statement = connection.prepareStatement(
-                    "SELECT p.*, b.quantity FROM Baskets b " +
-                            "JOIN Products p ON p.id = b.product_id WHERE order_id = 1;");
-            statement.setInt(1, order_id);
-            ResultSet results = statement.executeQuery();
-            //List<Product> products = productDao.getProducts();
-            while (results.next()) {
-                int quantity = results.getInt("quantity");
-                Product product = productDao.createProduct(results);
-                product.setAmount(quantity);
-                productsInBasket.add(product);
-            }
-            Basket basket = new Basket(order_id, productsInBasket);
-            results.close();
-            statement.close();
-            connection.close();
-            return basket;
-        } catch (SQLException e) {
-            throw new SQLException ();
-        }
-    }
-
     public List<Integer> getAllOrdersID() throws SQLException{
         List<Integer> ordersIDsList = new ArrayList<>();
         connect();
